ce683b9bfd52e495ba264f444687d2f4e7776cb9
Asukabai <3293775641@qq.com>
2025-09-03 20:37:34 +0800

    更新“联系我们”界面内容样式

--- src/view/ContactUs.vue
+++ src/view/ContactUs.vue
@@ -4,106 +4,52 @@
     <div class="container">
       <div class="container-fuild ContactUs-container">
         <div class="row">
-          <div class="col-xs-12 col-sm-12 col-md-6">
-            <form class="form-horizontal" role="form">
-              <div class="form-group">
-                <label for="name" class="col-sm-2 control-label">姓名</label>
-                <div class="col-sm-10 col-xs-12">
-                  <input type="text" class="form-control" id="name" placeholder="请输入名字">
-                </div>
-              </div>
-              <div class="form-group">
-                <label for="email" class="col-sm-2 control-label">邮箱</label>
-                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="email" placeholder="请输入邮箱">
-                </div>
-              </div>
-              <div class="form-group">
-                <label for="tel" class="col-sm-2 control-label">电话</label>
-                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="tel" placeholder="请输入电话">
-                </div>
-              </div>
-              <div class="form-group">
-                <label for="content" class="col-sm-2 control-label">内容</label>
-                <div class="col-sm-10">
-                  <textarea class="form-control" id="content" rows="8" placeholder="请输入内容"></textarea>
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                  <button type="submit" class="btn btn-default btn-block">提交</button>
-                </div>
-              </div>
-            </form>
-          </div>
-          <div class="col-xs-12 col-sm-12 col-md-6">
-            <div id="map" class="wow zoomIn"></div>
+          <div class="col-xs-12 col-sm-12 col-md-12">
+            <div style="font-size: 18px; line-height: 36px; margin: 40px 0;">
+              <p><strong>全国客户服务热线：</strong></p>
+              <p style="color: red;">400-616-0086</p>
+              <p><strong>E-mail：</strong>info@faithtech.cn</p>
+              <p><strong>总部地址：</strong>陕西省西安市长安区建工科技创业基地2号楼1单元5层6层</p>
+              <p><strong>山西分公司地址：</strong>长沙市高新开发区谷苑路229号海凭园10栋</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
 <script>
-import { WOW } from 'wowjs'
-import BMap from "BMap";
 export default {
   name: "ContactUs",
   data() {
     return {};
-  },
-  mounted() {
-    var map = new BMap.Map("map"); // 创建地图实例
-    var point = new BMap.Point(116.301841,40.156506); // 创建点坐标
-    map.centerAndZoom(point, 18); // 初始化地图，设置中心点坐标和地图级别
-    map.enableScrollWheelZoom(true); //开启鼠标滚轮缩放
-    var marker = new BMap.Marker(point); // 创建标注
-    map.addOverlay(marker); // 将标注添加到地图中
-    var opts = {
-      width: 50, // 信息窗口宽度
-      height: 10, // 信息窗口高度
-      title: "黄元公寓" // 信息窗口标题
-    };
-    var infoWindow = new BMap.InfoWindow(
-      "我的家",
-      opts
-    ); // 创建信息窗口对象
-    map.openInfoWindow(infoWindow, map.getCenter()); // 打开信息窗口
-    var wow = new WOW();
-    wow.init();
   }
 };
 </script>
+
 <style scoped>
 .banner {
   color: #fff;
   font-size: 30px;
   height: 150px;
   line-height: 150px;
-  background-image: url("../assets/img/banner_1.jpg");
+  background-image: url("../assets/img/banner_2.jpg");
   background-repeat: no-repeat;
   background-size: cover;
   background-attachment: scroll;
   background-position: center center;
 }
+
 .ContactUs-container {
   padding: 80px 0;
   transition: all ease 0.5s;
   box-sizing: border-box;
 }
-#map {
-  width: 100%;
-  height: 365px;
-}
-.row {
-  margin-right: 0;
-  margin-left: 0;
-}
+
 @media screen and (max-width: 997px) {
   .ContactUs-container {
     padding: 20px 0;
   }
 }
 </style>
-
